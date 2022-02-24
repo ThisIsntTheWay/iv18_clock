f822e37f63165198ab315d2614743a996b7a0648
The Man <ThisIsntTheWay@users.noreply.github.com>
2022-02-24 08:24:49 +0100

    Move enum, add untested functions.

--- src/tubeInterface.cpp
+++ src/tubeInterface.cpp
@@ -9,12 +9,20 @@ TubeInterface::TubeInterface() {
     digitalWrite(PIN_BLK, LOW);
 }
 
-void TubeInterface::Push() {
+void TubeInterface::Push(int8_t targetChar) {
+    int16_t bytesToPush = this->assemble(static_cast<Characters>(targetChar));
+
     digitalWrite(PIN_LOA, HIGH);
-    shiftOut(PIN_DIN, PIN_CLK, MSBFIRST, 0b00000000);
+    shiftOut(PIN_DIN, PIN_CLK, MSBFIRST, bytesToPush);
     digitalWrite(PIN_LOA, LOW);
 }
 
 void TubeInterface::Update(uint8_t Anode, int16_t Segments) {
     this->anode = Anode;
+}
+
+int16_t TubeInterface::assemble(Characters targetChar) {
+    //int16_t fullMask = 0b00000000000000000; // 17 zeroes
+    int16_t anodeMask = 1 << (8 + this->anode); // Anodes are OUT8 - OUT16 of SR
+    return anodeMask & targetChar;
 }
--- src/tubeInterface.h
+++ src/tubeInterface.h
@@ -8,27 +8,28 @@
 
 #include <Arduino.h>
 
+enum Characters : int8_t {
+    CHAR_0 = 0b1111110,
+    CHAR_1 = 0b0110000, // Right-hand
+    CHAR_2 = 0b1101101,
+    CHAR_3 = 0b1111001,
+    CHAR_4 = 0b0110011,
+    CHAR_5 = 0b1011011,
+    CHAR_6 = 0b0011111,
+    CHAR_7 = 0b1110000,
+    CHAR_8 = 0b1111111,
+    CHAR_9 = 0b1110011
+};
+
 class TubeInterface {
     public:
         TubeInterface();
 
-        void Push();
+        void Push(int8_t);
         void Update(uint8_t, int16_t);
     private:
+        int16_t assemble(Characters);
         uint8_t anode;
-
-        enum Characters : int8_t {
-            CHAR_0 = 0b1111110,
-            CHAR_1 = 0b0110000, // Right-hand
-            CHAR_2 = 0b1101101,
-            CHAR_3 = 0b1111001,
-            CHAR_4 = 0b0110011,
-            CHAR_5 = 0b1011011,
-            CHAR_6 = 0b0011111,
-            CHAR_7 = 0b1110000,
-            CHAR_8 = 0b1111111,
-            CHAR_9 = 0b1110011
-        };
 };
 
 #endif
